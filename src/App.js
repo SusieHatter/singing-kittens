9122f058587f76415cf093446384ca0c88bc3415
SusieHatter <susiivaldez@gmail.com>
2021-10-15 20:26:50 -0500

    Add playing beat

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+
+import "./App.css";
 
 import Cat from "./components/Cat/Cat";
 import Card from "./components/UI/Card";
 import Restart from "./components/UI/RestartButton";
 import Sequencer from "./components/Sequencer/Sequencer";
+import PlayIt from "./components/UI/PlayButton";
 
-import "./App.css";
+const BPM = 200;
+const NUM_BEATS_IN_SEQUENCE = 16;
 
 const sounds = {
   "white-cat": new Audio(`./assets/sounds/white-cat.wav`),
@@ -17,7 +21,7 @@ const sounds = {
 
 const playSound = (name) => {
   let sound = sounds[name];
-  sound.play();
+  sound.cloneNode().play();
 };
 
 const newSequence = (length) => {
@@ -29,15 +33,43 @@ const newSequence = (length) => {
 };
 
 const INITIAL_STATE = {
-  "white-cat": newSequence(16),
-  "black-cat": newSequence(16),
-  "stripped-cat": newSequence(16),
-  "orange-cat": newSequence(16),
-  "boop-cat": newSequence(16),
+  "white-cat": newSequence(NUM_BEATS_IN_SEQUENCE),
+  "black-cat": newSequence(NUM_BEATS_IN_SEQUENCE),
+  "stripped-cat": newSequence(NUM_BEATS_IN_SEQUENCE),
+  "orange-cat": newSequence(NUM_BEATS_IN_SEQUENCE),
+  "boop-cat": newSequence(NUM_BEATS_IN_SEQUENCE),
+};
+
+const getPlayingTracks = (sequencer, playingBeat) => {
+  const playingTracks = [];
+  for (let trackName in sequencer) {
+    const sequence = sequencer[trackName];
+    if (sequence[playingBeat]) {
+      playingTracks.push(trackName);
+    }
+  }
+  return playingTracks;
 };
 
 function App() {
   const [sequencer, setSequencer] = useState(INITIAL_STATE);
+  const [playingBeat, setPlayingBeat] = useState(0);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setPlayingBeat(
+        (playingBeat) => (playingBeat + 1) % NUM_BEATS_IN_SEQUENCE
+      );
+    }, (1000 * 60) / BPM);
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  const playingTracks = getPlayingTracks(sequencer, playingBeat);
+  for (let trackName of playingTracks) {
+    playSound(trackName);
+  }
 
   const resetSequencer = () => {
     setSequencer(INITIAL_STATE);
@@ -60,11 +92,16 @@ function App() {
     <div>
       <h1>Singing Kittens</h1>
       <Restart onClick={resetSequencer} />
+      <PlayIt />
       <Card>
         {Object.keys(sequencer).map((name) => (
           <Cat key={name} name={name} onClick={onClickCat(name)} />
         ))}
-        <Sequencer sequencer={sequencer} toggleBeat={toggleBeat} />
+        <Sequencer
+          sequencer={sequencer}
+          toggleBeat={toggleBeat}
+          playingBeat={playingBeat}
+        />
       </Card>
     </div>
   );
--- src/components/Sequencer/Sequencer.js
+++ src/components/Sequencer/Sequencer.js
@@ -2,23 +2,44 @@ import React from "react";
 
 import "./Sequencer.css";
 
-const Beat = ({ isOn, onClick }) => (
+const getBeatColor = (isOn, isPlaying) => {
+  if (isOn) {
+    if (isPlaying) {
+      return "lightgreen";
+    } else {
+      return "green";
+    }
+  } else {
+    if (isPlaying) {
+      return "yellow";
+    } else {
+      return "red";
+    }
+  }
+};
+
+const Beat = ({ isOn, isPlaying, onClick }) => (
   <div
     className="beat"
     onClick={onClick}
-    style={{ background: isOn ? "green" : "red" }}
+    style={{ background: getBeatColor(isOn, isPlaying) }}
   ></div>
 );
 
-const Sequence = ({ sequence, trackName, toggleBeat }) => (
+const Sequence = ({ sequence, trackName, toggleBeat, playingBeat }) => (
   <div>
     {sequence.map((beat, i) => (
-      <Beat key={i} onClick={() => toggleBeat(trackName, i)} isOn={beat} />
+      <Beat
+        key={i}
+        onClick={() => toggleBeat(trackName, i)}
+        isOn={beat}
+        isPlaying={i === playingBeat}
+      />
     ))}
   </div>
 );
 
-const Sequencer = ({ sequencer, toggleBeat }) => (
+const Sequencer = ({ sequencer, toggleBeat, playingBeat }) => (
   <div>
     {Object.entries(sequencer).map(([trackName, sequence]) => (
       <Sequence
@@ -26,6 +47,7 @@ const Sequencer = ({ sequencer, toggleBeat }) => (
         sequence={sequence}
         trackName={trackName}
         toggleBeat={toggleBeat}
+        playingBeat={playingBeat}
       />
     ))}
   </div>
